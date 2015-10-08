ea846262c110f007d0ceda3e4c45aeea5574f812
Martin Gruffydd Evans <martin.gruffydd@gmail.com>
2015-10-08 20:23:31 +0100

    commented shitty work

--- problem3.js
+++ problem3.js
@@ -1,18 +1,18 @@
-var mainNumber = 10;
-var threshold = mainNumber / 2;
+// var mainNumber = 10;
+// var threshold = mainNumber / 2;
 
-function isPrime(inputNumber) {
-	var prime = true;
-	var divisor = inputNumber;
-	while(prime != false && divisor > 0) {
-		divisor--;
-		if(inputNumber % divisor === 0) {
-			prime = false;
-		};
-		return prime;
-	};
-};
+// function isPrime(inputNumber) {
+// 	var prime = true;
+// 	var divisor = inputNumber;
+// 	while(prime != false && divisor > 0) {
+// 		divisor--;
+// 		if(inputNumber % divisor === 0) {
+// 			prime = false;
+// 		};
+// 		return prime;
+// 	};
+// };
 
-console.log(isPrime(8));
+// console.log(isPrime(8));
 
 document.getElementById("answer3").innerHTML = "start";
